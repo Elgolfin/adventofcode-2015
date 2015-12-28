a09e5a71acc4f2f665ce5552accd706cb50a2eab
Elgolfin <elgo@nicomputer.net>
2015-12-28 12:39:58 -0500

    Day08 Part 2 solution

--- AdventOfCode2015UnitTests/Day08_UnitTest.cs
+++ AdventOfCode2015UnitTests/Day08_UnitTest.cs
@@ -16,10 +16,26 @@ namespace AdventOfCode2015UnitTests
 
         [TestMethod]
         [TestCategory("Day08")]
+        public void string1_Encode()
+        {
+            string input = @"""""";
+            Assert.AreEqual(@"""\""\""""", input.EncodeSantaWay());
+        }
+
+        [TestMethod]
+        [TestCategory("Day08")]
         public void string2_LengthInMemory_Is_3()
         {
             string input = @"""abc""";
             Assert.AreEqual(3, input.LengthInMemory());
+        }
+
+        [TestMethod]
+        [TestCategory("Day08")]
+        public void string2_Encode()
+        {
+            string input = @"""abc""";
+            Assert.AreEqual(@"""\""abc\""""", input.EncodeSantaWay());
         }
 
         [TestMethod]
@@ -32,10 +48,26 @@ namespace AdventOfCode2015UnitTests
 
         [TestMethod]
         [TestCategory("Day08")]
+        public void string3_Encode()
+        {
+            string input = @"""aaa\""aaa""";
+            Assert.AreEqual(@"""\""aaa\\\""aaa\""""", input.EncodeSantaWay());
+        }
+
+        [TestMethod]
+        [TestCategory("Day08")]
         public void string4_LengthInMemory_Is_1()
         {
             string input = @"""\x27""";
             Assert.AreEqual(1, input.LengthInMemory());
+        }
+
+        [TestMethod]
+        [TestCategory("Day08")]
+        public void string4_Encode()
+        {
+            string input = @"""\x27""";
+            Assert.AreEqual(@"""\""\\x27\""""", input.EncodeSantaWay());
         }
 
         [TestMethod]
@@ -53,6 +85,23 @@ namespace AdventOfCode2015UnitTests
             int diff = input1.Length + input2.Length + input3.Length + input4.Length;
             diff -= input1.LengthInMemory() + input2.LengthInMemory() + input3.LengthInMemory() + input4.LengthInMemory();
             Assert.AreEqual(12, diff);
+        }
+
+        [TestMethod]
+        [TestCategory("Day08")]
+        public void Diff_Is_19()
+        {
+            string input1 = @"""""";
+            Assert.AreEqual(6, input1.EncodeSantaWay().Length);
+            string input2 = @"""abc""";
+            Assert.AreEqual(9, input2.EncodeSantaWay().Length);
+            string input3 = @"""aaa\""aaa""";
+            Assert.AreEqual(16, input3.EncodeSantaWay().Length);
+            string input4 = @"""\x27""";
+            Assert.AreEqual(11, input4.EncodeSantaWay().Length);
+            int diff = input1.EncodeSantaWay().Length + input2.EncodeSantaWay().Length + input3.EncodeSantaWay().Length + input4.EncodeSantaWay().Length;
+            diff -= input1.Length + input2.Length + input3.Length + input4.Length;
+            Assert.AreEqual(19, diff);
         }
     }
 }
