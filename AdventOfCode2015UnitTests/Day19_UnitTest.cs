47dbb16da84bed01688bcb1dcf7f81d0dcea184e
Elgolfin <elgo@nicomputer.net>
2016-01-02 11:50:13 -0500

    Day19 Part 1 solution

--- AdventOfCode2015UnitTests/Day19_UnitTest.cs
+++ AdventOfCode2015UnitTests/Day19_UnitTest.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode2015UnitTests
         {
             string input = "HOH";
             MoleculeReplacement molRepl = new MoleculeReplacement(input, "H", "HO");
-            Assert.AreEqual(2, molRepl.GenerateNewMolecule().Count);
+            Assert.AreEqual(2, molRepl.GenerateNewMolecules().Count);
         }
 
         [TestMethod]
@@ -21,7 +21,7 @@ namespace AdventOfCode2015UnitTests
         {
             string input = "HOH";
             MoleculeReplacement molRepl = new MoleculeReplacement(input, "H", "OH");
-            Assert.AreEqual(2, molRepl.GenerateNewMolecule().Count);
+            Assert.AreEqual(2, molRepl.GenerateNewMolecules().Count);
         }
 
         [TestMethod]
@@ -30,7 +30,23 @@ namespace AdventOfCode2015UnitTests
         {
             string input = "HOH";
             MoleculeReplacement molRepl = new MoleculeReplacement(input, "O", "HH");
-            Assert.AreEqual(1, molRepl.GenerateNewMolecule().Count);
+            Assert.AreEqual(1, molRepl.GenerateNewMolecules().Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Day19")]
+        public void Calibrate_Machine_For_HOH_returns_4()
+        {
+            string input = "HOH";
+            MoleculeConstructionMachine machine = new MoleculeConstructionMachine(input);
+            MoleculeReplacement molRepl_1 = new MoleculeReplacement(input, "H", "HO");
+            MoleculeReplacement molRepl_2 = new MoleculeReplacement(input, "H", "OH");
+            MoleculeReplacement molRepl_3 = new MoleculeReplacement(input, "O", "HH");
+            machine.AddMoleculeReplacement(molRepl_1);
+            machine.AddMoleculeReplacement(molRepl_2);
+            machine.AddMoleculeReplacement(molRepl_3);
+            int newPossiblesMolecules = machine.Calibrate();
+            Assert.AreEqual(4, newPossiblesMolecules);
         }
     }
 }
--- Day19.cs
+++ Day19.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace AdventOfCode2015
 {
@@ -11,9 +12,63 @@ namespace AdventOfCode2015
     {
         public static void MedicineForRudolph_Part1()
         {
-
+            string originalMolecule = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF";
+            MoleculeConstructionMachine machine = new MoleculeConstructionMachine(originalMolecule);
+            using (StreamReader sr = new StreamReader("../../Day19.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string input = sr.ReadLine();
+                    string from = String.Empty;
+                    string to = String.Empty;
+                    string pattern = @"^(?<from>[a-z]+) => (?<to>[a-z]+)$";
+                    Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                    Match match = regex.Match(input);
+                    if (match.Success)
+                    {
+                        from = match.Groups["from"].Value;
+                        to = match.Groups["to"].Value;
+                        MoleculeReplacement mr = new MoleculeReplacement(originalMolecule, from, to);
+                        machine.AddMoleculeReplacement(mr);
+                    }
+                }
+                int results = machine.Calibrate();
+                Console.WriteLine(String.Format("{0} distinct molecules can be created.", results));
+            }
         }
 
+    }
+
+    public class MoleculeConstructionMachine
+    {
+        List<MoleculeReplacement> replacements = new List<MoleculeReplacement>();
+        List<string> possiblesMoleculesReplacements = new List<string>();
+        public string OriginalMolecule;
+
+        public MoleculeConstructionMachine(string originalMolecule)
+        {
+            OriginalMolecule = originalMolecule;
+        }
+
+        public void AddMoleculeReplacement (MoleculeReplacement mr)
+        {
+            if (!replacements.Contains(mr))
+            {
+                replacements.Add(mr);
+            }
+        }
+
+        public int Calibrate()
+        {
+            foreach (MoleculeReplacement mr in replacements)
+            {
+                possiblesMoleculesReplacements.AddRange(mr.GenerateNewMolecules());
+            }
+            List<string> tmp = new List<string>();
+            tmp.AddRange(possiblesMoleculesReplacements.Distinct());
+            possiblesMoleculesReplacements = tmp;
+            return possiblesMoleculesReplacements.Count;
+        }
     }
 
     public class MoleculeReplacement
@@ -21,6 +76,7 @@ namespace AdventOfCode2015
         public string From;
         public string To;
         public string OriginalMolecule;
+        public List<string> ReplacedPossibleMolecules = new List<string>();
         public MoleculeReplacement(string molelculeToReplace, string from, string to)
         {
             OriginalMolecule = molelculeToReplace;
@@ -28,7 +84,7 @@ namespace AdventOfCode2015
             To = to;
         }
 
-        public List<string> GenerateNewMolecule ()
+        public List<string> GenerateNewMolecules ()
         {
             string pattern = String.Format("({0})", From);
             List<string> replacedMolecules = new List<string>();
